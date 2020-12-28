8bffb26d9141a5f546b84e144def6c7485e07fbc
Tobias Gatschet <saibottob.gatschet@gmail.com>
2020-12-28 17:53:04 +0100

    add webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path")
+module.exports = {
+  entry: './sass/main.scss',
+  output: {
+    path: path.resolve(__dirname, 'lib')
+  },
+  module: {
+    rules: [
+      // Extracts the compiled CSS from the SASS files defined in the entry
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // Interprets CSS
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'sass-loader' // 将 Sass 编译成 CSS
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    // Where the compiled SASS is saved to
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+        title : 'Tobias Gatschet',
+        filename: 'home.html',
+        template: 'index.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true
+        }
+      }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          safe: true
+        }
+      })
+    ]
+  },
+};
