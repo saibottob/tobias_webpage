981367d20e8b940c99d7d5e22f006772efa349bb
saibottob <saibottob.gatschet@gmail.com>
2025-06-29 16:35:33 +0200

    Refactor Eleventy configuration, add blog collection, and update homepage with personal introduction and latest blog entries; create politics page and styles

--- webseite_2024/.eleventy.js
+++ webseite_2024/.eleventy.js
@@ -1,21 +1,25 @@
-module.exports = function(eleventyConfig) {
-    // Copy the `img` and `css` folders to the output
-    eleventyConfig.addPassthroughCopy("src/assets/");
-    eleventyConfig.addPassthroughCopy("src/css/");
-    eleventyConfig.addPassthroughCopy("src/js/");
+module.exports = function (eleventyConfig) {
+  // Copy the `img` and `css` folders to the output
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addPassthroughCopy("src/js/");
 
-    eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addWatchTarget("src/css/");
 
-    // You can return your Config object (optional)
-    return {
-        dir: {
-            input: "src",
-            includes: "_includes",
-            output: "dist"
-        },
-        templateFormats: ["njk", "html", "md"],
-        markdownTemplateEngine: "njk",
-        HTMLTemplateElement: "njk",
-        dataTemenplateEngine: "njk"
-    };
+  eleventyConfig.addCollection("blog", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/*.md");
+  });
+
+  // You can return your Config object (optional)
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "dist",
+    },
+    templateFormats: ["njk", "html", "md"],
+    markdownTemplateEngine: "njk",
+    HTMLTemplateElement: "njk",
+    dataTemenplateEngine: "njk",
+  };
 };
---
+++ webseite_2024/src/blog/mein-erster-post.md
@@ -0,0 +1,8 @@
+---
+title: Mein erster Blogpost
+date: 2024-06-29
+description: Das ist mein erster Blogeintrag!
+image: /assets/images/Portrait.jpg
+---
+
+Hier steht der Inhalt deines Blogposts.
