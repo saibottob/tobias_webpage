54995cd6a9ca34b6557b82ae7823c70b89dfe1c9
saibottob <saibottob.gatschet@gmail.com>
2026-04-26 01:47:29 +0200

    update so dasses wieder funktioniert

--- webseite_2024/.eleventy.js
+++ webseite_2024/.eleventy.js
@@ -3,6 +3,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css/");
   eleventyConfig.addPassthroughCopy("src/js/");
 
+  eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
+
+  eleventyConfig.addFilter("date", function (value, format) {
+    const d = new Date(value);
+    if (!format || format === "dd.MM.yyyy") {
+      return d.toLocaleDateString("de-CH", { day: "2-digit", month: "2-digit", year: "numeric" });
+    }
+    return d.toLocaleDateString("de-CH");
+  });
+
   eleventyConfig.addWatchTarget("src/css/");
 
   eleventyConfig.addCollection("blog", function (collectionApi) {
